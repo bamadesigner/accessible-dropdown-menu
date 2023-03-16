b5dd88ed9618330c61e4198c90d789f22c922c00
Rachel Cherry <rachel@bamadesigner.com>
2023-03-16 15:26:07 -0400

    Making the horiz menu wrap, and adding some descriptions for the github page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,12 @@
 
 <svelte:head>
 	<title>Accessible dropdown menu</title>
+	<meta name="description" content="HTML, CSS, and JavaScript for an accessible dropdown menu" />
 </svelte:head>
 
 <h1>Accessible dropdown menu</h1>
+<p>The following is an accessible "horizontal" dropdown menu. It is not built (yet) for "mobile, or smaller screens.</p>
+
 {#if !menu}
 	<p>There is no menu information.</p>
 {:else}
